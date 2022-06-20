6310fa6a769a1bb7bcd61e8eca91493e72e4bdd3
dilshan27 <damithadilshan27@gmail.com>
2022-06-20 17:02:08 +0530

    update

--- src/controller/AccountFormController.java
+++ src/controller/AccountFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class AccountFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+    public JFXPasswordField txtConfirmPassword;
+
+
+    public void initialize() {
+        txtUserName.setEditable(false);
+        txtPassword.setEditable(false);
+        txtConfirmPassword.setEditable(false);
+    }
+
+    public void btnEditOnAction(ActionEvent actionEvent) {
+        txtUserName.setEditable(true);
+        txtPassword.setEditable(true);
+        txtConfirmPassword.setEditable(true);
+        txtUserName.requestFocus();
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+        txtUserName.setEditable(false);
+        txtPassword.setEditable(false);
+        txtConfirmPassword.setEditable(false);
+    }
 }
